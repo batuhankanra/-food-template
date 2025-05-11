f673ba01859d804a6c9b109cf5b27c130c096629
batuhan kanra <batuhan_kanra_60@hotmail.com>
2025-05-11 16:22:15 +0300

    banner uptaded

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router"
 import Header from "./header"
 import Footer from "./footer"
+import Banner from "../components/banner"
+
 
 
 const MainLayout = () => {
@@ -8,7 +10,7 @@ const MainLayout = () => {
     <div className="w-full h-full flex flex-col  justify-between">
       <Header />
       
-      
+        <Banner />
       <div className="my-20">
         <Outlet />
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import Banner from "../../components/banner"
 import Cart from "../../components/cart"
 import DinnerToday from "../../components/dinnerToday"
 import TodaysRecipe from "../../components/todaysRecipe"
@@ -8,7 +7,7 @@ import TodaysRecipe from "../../components/todaysRecipe"
 const Home = () => {
   return (
     <section className='mt-6 container mx-auto flex flex-col gap-y-10'>
-        <Banner />
+      
       
         <Cart />
         <DinnerToday />
